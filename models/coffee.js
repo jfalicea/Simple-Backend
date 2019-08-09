28a4654351e37229a96b34fdd6a5187a0759434b
Jonathan <jonathan@boldideas.net>
2019-08-09 13:39:23 -0400

    Added the Coffee class.

---
+++ models/coffee.js
@@ -0,0 +1,30 @@
+const db = require("./conn");
+
+class Coffee{
+    constructor(id, name, orders, size){
+        this.id = id; 
+        this.name = name; 
+        this.orders = orders;
+        this.size = size;
+    }
+    static getById(id){
+        return db.one(`select*from coffee where id=${id}`)
+            .then((oneCoffeeData)=>{
+                const coffeeInstance = new Coffee(oneCoffeeData.id, oneCoffeeData.name, oneCoffeeData.orders, oneCoffeeData.size);
+                return coffeeInstance
+            })
+            .catch(()=>{
+                return null;
+            });
+    }
+    save(){
+        return db.result(`
+        update coffee set
+        name = ${this.name},
+        orders = ${this.orders},
+        size = ${this.size},
+        where id=${this.id}`
+        )
+    }
+}
+module.exports = Coffee
