8d3cc3e99066de8208017be59c5d39349c2c961f
Jonathan <jonathan@boldideas.net>
2019-08-09 10:53:15 -0400

    Created seed file for coffee app.

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO coffee (name, orders, size) VALUES
+    ('Amy', 'Coffee Black', 'Grande'),
+    ('Andy', 'Americano Extra Cream Extra Sugar', 'Venti'),
+    ('Aretha', 'Cappuccino Light Form', 'Tall'),
+    ('Alex', 'Breve Lata Extra Sugar', 'ExtraGrande');
