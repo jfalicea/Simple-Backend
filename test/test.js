7c20d56b94304cb88acb11bf7e207123e606d94c
Jonathan <jonathan@boldideas.net>
2019-08-09 11:23:36 -0400

    Added TDD for Coffee App

---
+++ test/test.js
@@ -0,0 +1,27 @@
+const assert = require('assert'); 
+const chai = require ('chai');
+const expect = chai.expect; 
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised).should();
+
+const Coffee = require('../models/coffee')
+
+describe('Sanity Check', ()=>{
+    it('Should be 2', ()=>{
+        assert.equal(2, 1+1);
+        expect(1+1).to.equal(2);
+    });
+
+    it('Should be 3', ()=>{
+        assert.equal(3, 2+1);
+        expect(2+1).to.equal(3);
+    });
+
+});
+
+
+describe('Coffee Model', async()=>{
+    const  coffeeData = await Coffee.getByID(3);
+    console.log(coffeeData)
+    expect(coffeeData).to.be.an.instanceOf(Coffee);    
+});
